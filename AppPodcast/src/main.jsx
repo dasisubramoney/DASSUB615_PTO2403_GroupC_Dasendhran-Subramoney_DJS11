77adf24a1affd044e8fae6eee67cb7f1d92e0ddf
Dasendhran Subramoney <das.subramoney@gmail.com>
2025-02-27 22:49:21 +0200

    Added the audio player globally

--- AppPodcast/src/main.jsx
+++ AppPodcast/src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { AudioProvider } from './components/Layout/AudioContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <AudioProvider>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </AudioProvider>,
 )
